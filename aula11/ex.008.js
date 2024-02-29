7f2467251b06d4b61f1d4e3733525f84d63acdc7
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-02-29 20:34:16 -0300

    Aula 11 – Condições (Parte 1)
    Você sabe usar o comando if no JavaScript? Sabe diferenciar condições simples e condições compostas? Sabe como criar uma condição usando JavaScript? Sabe como instalar e configurar a extensão Node Exec no Visual Studio Code?

---
+++ aula11/ex.008.js
@@ -0,0 +1,10 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 50) {
+    console.log(`Você ultrapassou o limite de velocidade.  MULTADO`)
+} else {
+    console.log(`Se é zika mlk, está dentro da velocidade`)
+}
+
+
+console.log(`Dirija sempre usando cinto de segurança`)
