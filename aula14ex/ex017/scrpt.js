553535b2fc4fcce672996a537a6927cf1e008ed2
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-03-05 22:43:13 -0300

    Exercícios JavaScript (Parte 6)

--- aula14ex/ex017/scrpt.js
+++ aula14ex/ex017/scrpt.js
@@ -1,1 +1,21 @@
-alert (`olá`)
+function gerar() {
+
+    var num = document.getElementById('txtnum')
+    var tab = document.getElementById('seltab')
+   
+    
+
+    if( num.value.length !== 0) {
+        var n = Number(num.value)
+        
+    } else {
+        window.alert('Por favor digite um numero') }
+    tab.innerHTML=""
+    for ( var i = 1; i <=10; i++){
+        
+        let item = document.createElement('option')
+        item.text = `${n} x ${i} = ${n*i}`
+        tab.appendChild(item)
+        
+    }
+}
